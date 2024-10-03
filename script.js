f35c7b30aba44af655c1c82421872617732e0258
randomprojectss <santongxv@gmail.com>
2024-10-03 20:06:14 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Dark/Light Mode Toggle
-const themeToggle = document.getElementById('themeToggle');
-let isDarkMode = false;
-
-themeToggle.addEventListener('click', () => {
-    isDarkMode = !isDarkMode; // Toggle the mode
-    document.body.classList.toggle('dark-mode', isDarkMode); // Add/remove dark mode class
-    themeToggle.textContent = isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'; // Update button text
-});
